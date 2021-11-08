bf4bd16b04135330dd80f3161db742ec344b0ba0
QuinntyneBrown <quinntynebrown@gmail.com>
2021-11-07 23:01:24 -0500

    refactor

--- src/Biblical.Api/Models/Book.cs
+++ src/Biblical.Api/Models/Book.cs
@@ -5,12 +5,12 @@ namespace Biblical.Api.Models
 {
     public class Book
     {
-        public Guid BookId { get; set; }
-        public string Name { get; set; }
+        public Guid BookId { get; private set; }
+        public string Name { get; private set; }
 
         public Book(string name)
         {
-            if(_isValidBookOfTheBible(name) == false)
+            if (_isValidBookOfTheBible(name) == false)
             {
                 throw new Exception("Not a valid book of the bible!");
             }
@@ -18,21 +18,14 @@ namespace Biblical.Api.Models
             Name = name;
         }
 
-        private bool _isValidBookOfTheBible(string name)
+        private Book()
         {
-            string[] booksOfTheBible = new string[2] {
+
+        }
+
+        private bool _isValidBookOfTheBible(string name) => new string[2] {
                 "Genesis",
                 "Exodus"
-            };
-
-            if(booksOfTheBible.Contains(name))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+            }.Contains(name);
     }
 }
